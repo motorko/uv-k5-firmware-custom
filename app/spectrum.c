cd13ccacafe94e78d5bf84a50b97dac78301aafb
Vitalii Motorko <vitalii@mgroupweb.com>
2025-02-15 12:48:08 +0200

    Fix blacklist check

--- app/spectrum.c
+++ app/spectrum.c
@@ -679,7 +679,7 @@ static void Blacklist() {
 #ifdef ENABLE_SCAN_RANGES
 static bool IsBlacklisted(uint16_t idx)
 {
-  for(uint8_t i = 0; i < ARRAY_SIZE(blacklistFreqs); i++)
+  for(uint8_t i = 0; i < blacklistFreqsIdx; i++)
     if(blacklistFreqs[i] == idx)
       return true;
   return false;
