d9b3d12c340c1bbca9eaf433961a76fe1d9bdb29
Vitalii Motorko <vitaliimotorko@gmail.com>
2025-03-23 13:09:38 +0200

    Add SETTINGS_SetVfoFrequency function for spectrum copy VFO feature

--- frequencies.h
+++ frequencies.h
@@ -41,7 +41,7 @@ typedef enum  {
 	BAND_N_ELEM
 } FREQUENCY_Band_t;
 
-extern const freq_band_table_t frequencyBandTable[];
+extern const freq_band_table_t frequencyBandTable[7];
 
 typedef enum {
 // standard steps
@@ -74,7 +74,7 @@ typedef enum {
 } STEP_Setting_t;
 
 
-extern const uint16_t gStepFrequencyTable[];
+extern const uint16_t gStepFrequencyTable[STEP_N_ELEM];
 
 #ifdef ENABLE_NOAA
 	extern const uint32_t NoaaFrequencyTable[10];
